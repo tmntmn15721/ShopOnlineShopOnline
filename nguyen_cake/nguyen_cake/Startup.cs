331391ce04c08b5415bd8470438e08293aa81ede
tmntmn15721 <nguyen15072001@gmail.com>
2021-04-14 15:51:05 +0700

    Upload Project first time

---
+++ nguyen_cake/nguyen_cake/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nguyen_cake.Startup))]
+namespace nguyen_cake
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
